e3c80e6126a362bb0d03da4d053e6ffb8bbc364d
Laman Isgandarova <104322290+laman-isg@users.noreply.github.com>
2023-04-12 00:06:31 +0400

    Add files via upload

---
+++ RandomColors/ColorsDetailVC.swift
@@ -0,0 +1,30 @@
+//
+//  ColorsDetailVC.swift
+//  RandomColors
+//
+//  Created by Laman Isgandarova on 24.03.23.
+//
+
+import UIKit
+
+class ColorsDetailVC: UIViewController {
+
+    var color: UIColor?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = color ?? .blue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
